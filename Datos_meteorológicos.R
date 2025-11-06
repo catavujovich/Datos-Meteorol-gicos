8e30ea8dc616b7bfc2e866b33ec51fc6f147d3f4
catalinavujovich <catavujovich17@gmail.com>
2025-11-06 19:16:48 -0300

    Agrego función tabla_resumen_temperatura

--- Datos_meteorológicos.R
+++ Datos_meteorológicos.R
@@ -23,12 +23,26 @@ leer_estacion <- function(id_estacion, ruta_archivo) {
   return(datos_estacion)
 }
 
-# Metadatos
-metadatos <- leer_estacion("metadatos_completos", "datos/metadatos.csv")
 
-# Estaciones meteorológicas
-NH0472 <- leer_estacion("NH0472", "datos/NH0472.csv")
-NH0910 <- leer_estacion("NH0910", "datos/NH0910.csv")
-NH0046 <- leer_estacion("NH0046", "datos/NH0046.csv")
-NH0098 <- leer_estacion("NH0098", "datos/NH0098.csv")
-NH0437 <- leer_estacion("NH0437", "datos/NH0437.csv")
+library(dplyr)
+library(tidyr)
+
+tabla_resumen_temperatura <- function(...) {
+  
+  # Combinar una o más estaciones
+  datos <- bind_rows(...)
+  
+  # Crear tabla de resumen por id (columna que identifica la estación)
+    resumen <- datos %>%
+    group_by(id) %>%
+    summarise(
+      variable = "temperatura_abrigo_150cm",
+      media = mean(temperatura_abrigo_150cm, na.rm = TRUE),
+      mediana = median(temperatura_abrigo_150cm, na.rm = TRUE),
+      minimo = min(temperatura_abrigo_150cm, na.rm = TRUE),
+      maximo = max(temperatura_abrigo_150cm, na.rm = TRUE),
+    )
+  
+  return(resumen)
+}
+
